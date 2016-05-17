0c1238cc1af185bbe232d34543ee29d19c8130ac
Erica <erica@Ericas-MacBook-Air.local>
2016-05-17 10:57:53 -0400

    dict

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -16,4 +16,37 @@
  
  */
 
+
+
+-(NSArray*)namesForIceCream:(NSString *)iceCream {
+    
+    NSDictionary *iceCreamByName = @{ @"Joe": @"Peanut Butter and Chocolate",
+                                        
+                                        @"Tim" : @"Natural Vanilla",
+                                        
+                                        @"Sophie" : @"Mexican Chocolate",
+                                        
+                                        @"Deniz" : @"Natural Vanilla",
+                                        
+                                        @"Tom" : @"Mexican Chocolate",
+                                        
+                                        @"Jim" : @"Natural Vanilla",
+                                        
+                                        @"Mark" : @"Cookies 'n Cream"};
+    
+    
+    NSDictionary *countsByIceCream = @{ @"Peanut Butter and Chocolate" : @1     ,
+                          @"Natural Vanilla"             : @3     ,
+                          @"Mexican Chocolate"           : @2     ,
+                          @"Cookies 'n Cream"            : @1    };
+    
+    
+    return nil;
+}
+
+-(NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    
+    return nil;
+}
+
 @end
